7f610c5651edb74f5545dff79d48c40967f6ec96
shenjiajun <365shenjiajun@sina.com>
2019-03-13 20:35:47 +0800

    add first test

---
+++ 2019313.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import time
+# see what you see now
+print("hello word")
+#name = input("what is your name")
+name = "sjj"
+print("hello,"+name+"!")
+# get the time of day 
+print ("Start : %s" % time.ctime())
+time.sleep(3)
+print ("End : %s" % time.ctime())
+#sting show
+print('let\'s go') 
+print("let's go too")
