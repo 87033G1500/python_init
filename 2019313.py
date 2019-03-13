add7f50c0cff80a57f2cac78ed0d531542692fb7
shenjiajun <365shenjiajun@sina.com>
2019-03-13 21:14:29 +0800

    temp 2

--- 2019313.py
+++ 2019313.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 import time
 # see what you see now
-print("hello word")
-#name = input("what is your name")
-name = "sjj"
+print("hello \
+    word")
+name = input("what is your name")
+#name = "sjj"
 print("hello,"+name+"!")
 # get the time of day 
 print ("Start : %s" % time.ctime())
@@ -12,3 +13,8 @@ print ("End : %s" % time.ctime())
 #sting show
 print('let\'s go') 
 print("let's go too")
+dict = {'runoob': 'runoob.com', 'google': 'google.com'};
+print (repr ('hello. world!'))
+print(repr(dict))
+print(repr(10000))
+print (str(10000))
